bf46b8a1bf9a97ff70e768d13e446c26bc206ea8
Destruction-Studios <founderofbeans@gmail.com>
2025-01-07 19:25:24 -0500

    Initial commit

---
+++ src/main/java/net/ds/musicdiscs/MusicDiscsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.ds.musicdiscs;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MusicDiscsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
